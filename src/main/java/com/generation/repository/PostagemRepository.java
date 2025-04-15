8ae3368bd25777893474c60dfe9ffdb76231615c
Bruno Daniel <brunodanielferreira314@gmail.com>
2025-04-14 23:48:17 -0300

    Iniciando projeto de blog pessoal

---
+++ src/main/java/com/generation/repository/PostagemRepository.java
@@ -0,0 +1,11 @@
+package com.generation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+	
+
+}
